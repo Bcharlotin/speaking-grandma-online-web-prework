de1fe2b01f6138d04d5671b4a6ed40a510f25ebe
biancacharlotin <biancacharlotin@gmail.com>
2018-11-02 16:12:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-end
-  if phrase== phrase.upcase
+  
+  if speak_to_grandma(phrase)== speak_to_grandma(phrase).upcase
       puts ("NO, NOT SINCE 1938!")
   
-  elsif phrase== "I LOVE YOU GRANDMA!"
+  elsif speak_to_grandma(phrase)== "I LOVE YOU GRANDMA!"
       puts ("I LOVE YOU TOO PUMPKIN!")
   else 
     puts ("HUH?! SPEAK UP, SONNY!")
