5459a4f564179af66a0a304afa76aa520c45baf8
biancacharlotin <biancacharlotin@gmail.com>
2018-11-02 16:20:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   
-  if peak_to_grandma(phrase)== "I LOVE YOU GRANDMA!"
+  if speak_to_grandma(phrase)== "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
